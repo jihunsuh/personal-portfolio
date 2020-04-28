3e81c4fe3c79d60c823e15bb282386db6f0589ac
서지훈 <jihunsuh12@gmail.com>
2020-04-28 11:31:36 +0900

    build project structure - divide with client and server

---
+++ server/public/precache-manifest.6d517a300120e86a8d35885ac3a09cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199125a5e29e854ea6741a3a9e1badab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91a7e47bde7716afe489",
+    "url": "/static/css/main.d551caa7.chunk.css"
+  },
+  {
+    "revision": "8579cfaf6a695eb0e708",
+    "url": "/static/js/2.6eee646b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6eee646b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a7e47bde7716afe489",
+    "url": "/static/js/main.0508fc05.chunk.js"
+  },
+  {
+    "revision": "2e04a1bd3eae7e2f23ae",
+    "url": "/static/js/runtime-main.bb007eb6.js"
+  }
+]);
